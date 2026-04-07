37658fe50d7743c99ef12e59a4030549be0c972f
dlgkrals <dlgkrals1136@naver.com>
2026-04-07 15:15:12 +0900

    feat: 업무 일지 생성 기능에 날짜 선택 추가

--- src/main/java/com/ict/wiki/report/controller/DailyReportController.java
+++ src/main/java/com/ict/wiki/report/controller/DailyReportController.java
@@ -5,6 +5,7 @@ import com.ict.wiki.login.service.UserService;
 import com.ict.wiki.report.service.DailyReportService;
 import com.ict.wiki.security.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -26,14 +27,15 @@ public class DailyReportController {
 
     @GetMapping("/daily")
     public ResponseEntity<byte[]> downloadDailyReport(
-            @AuthenticationPrincipal CustomUserDetails userDetails) throws Exception {
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) throws Exception {
 
         User user = userService.findById(userDetails.getId());
-        LocalDate today = LocalDate.now();
+        LocalDate targetDate = (date != null) ? date : LocalDate.now();
 
-        byte[] excelBytes = dailyReportService.generateReport(user, today);
+        byte[] excelBytes = dailyReportService.generateReport(user, targetDate);
 
-        String filename = today.format(DateTimeFormatter.ofPattern("MM월dd일"))
+        String filename = targetDate.format(DateTimeFormatter.ofPattern("MM월dd일"))
                 + "_" + user.getName() + "_업무일지.xls";
         String encodedFilename = URLEncoder.encode(filename, StandardCharsets.UTF_8);
 
